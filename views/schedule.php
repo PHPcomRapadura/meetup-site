985b7d6e73f560c83f6a8a4837634cb01a0d0d12
wesley <wesleyhu.informatica@gmail.com>
2018-10-18 15:06:48 -0300

    Alteração da programação e local

--- views/schedule.php
+++ views/schedule.php
@@ -20,70 +20,50 @@
 
                 <tbody class="font-blue">
                   <tr>
-                    <td>07:30 - 08:00</td>
-                    <td>Café e Credenciamento</td>
+                    <td>07:40 - 08:10</td>
+                    <td>Credenciamento</td>
                     <td>Comunidade PHP com Rapadura</td>
                   </tr>
                   <tr>
-                    <td>08:20 - 08:40</td>
+                    <td>08:15 - 08:30</td>
                     <td>Abertura</td>
-                    <td>Comunidade PHP com Rapadura e UNIFOR</td>
-                  </tr>
-                  <tr>
-                    <td>08:45  - 09:25</td>
-                    <td>Bora falar de Work Queues!</td>
-                    <td>Luítame de Oliveira</td>
-                  </tr>
-                  <tr>
-                    <td>09:30 - 10:10</td>
-                    <td>Com grandes códigos vêm grandes gambiarras</td>
-                    <td>Aline Chaves</td>
-                  </tr>
-                  <tr>
-                    <td>10:15 - 10:55</td>
-                    <td>Existe dois tipos de sistemas, os que foram invadidos<br> e os que pensam que não foram invadidos!</td>
-                    <td>Walderlan Sena</td>
-                  </tr>
-                  <tr>
-                    <td>11:00 - 11:40</td>
-                    <td>Keynote Internacional</td>
-                    <td>Cyrille Grandval (Darkmira)</td>
-                  </tr>
-                  <tr>
-                    <td>11:40 - 13:30</td>
-                    <td>Intervalo para almoço</td>
-                    <td>Todo mundo!</td>
-                  </tr>
-                  <tr>
-                    <td>13:35 - 14:05</td>
-                    <td>Construindo estruturas complexas com serialização de obejtos</td>
-                    <td>Alef Castelo</td>
-                  </tr>
-                  <tr>
-                    <td>14:10 - 14:50</td>
-                    <td>PHP Dependency injection container</td>
-                    <td>Jholdy Phillip</td>
-                  </tr>
-                  <tr>
-                    <td>14:50 - 15:30</td>
-                    <td>Intervalo para coffee break</td>
                     <td>Comunidade PHP com Rapadura</td>
                   </tr>
                   <tr>
-                    <td>15:35 - 16:05</td>
-                    <td>Refactoring: Porque?, quando? e como?</td>
-                    <td>Ian Rodrigues</td>
+                    <td>08:35  - 09:15</td>
+                    <td>Palestra 01</td>
+                    <td></td>
                   </tr>
                   <tr>
-                    <td>16:10 - 16:50</td>
-                    <td>Trabalhando com paralelismo e concorrência em PHP</td>
-                    <td>Vidal Vasconcelos</td>
+                    <td>09:20 - 10:00</td>
+                    <td>Palestra 02</td>
+                    <td></td>
                   </tr>
                   <tr>
-                    <td>17:00 - 17:30</td>
-                    <td>Encerramento: Agradecimentos e sorteio de brindes</td>
+                    <td>10:05 - 10:45</td>
+                    <td>Coffee Break</td>
                     <td>Comunidade PHP com Rapadura</td>
-                  </tr> 
+                  </tr>
+                  <tr>
+                    <td>10:50 - 11:30</td>
+                    <td>Palestra 03</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>11:35 - 12:15</td>
+                    <td>Palestra 04</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>12:20 - 12:40</td>
+                    <td>Joguinho de Perguntas</td>
+                    <td>Comunidade PHP com Rapadura</td>
+                  </tr>
+                  <tr>
+                    <td>12:45 - 13:00</td>
+                    <td>Encerramento e Brindes</td>
+                    <td>Comunidade PHP com Rapadura</td>
+                  </tr>
                 </tbody>
               </table>
               
