be770b2a013fa669da492a98841d7ea6755ec58c
Alessandro Feitoza <eu@alessandrofeitoza.eu>
2018-10-25 13:56:27 -0300

    improvement: Adicionando botão para ir pro site da sympla, iframe não tá funfando

--- views/base.php
+++ views/base.php
@@ -60,7 +60,7 @@
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="assets/js/materialize.js"></script>
     <script src="assets/js/init.js"></script>
-    <script src="https://www.sympla.com.br/js/sympla.widget-pt.js/288555"></script>
+    
 
 </body>
 </html>
